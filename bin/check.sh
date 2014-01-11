6661dc789dd760df667608a5126fb5e49f690746
Thomas Levine <occurrence@thomaslevine.com>
2014-01-11 19:45:05 +0000

    feedback if everything is good

--- bin/check.sh
+++ bin/check.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+all_well=true
+
 for file in $(ls); do
   if test $file = .git ||
     (test -f $file && echo $file | grep '.key$' > /dev/null ) ||
@@ -9,12 +11,19 @@ for file in $(ls); do
     (test -f $file && test $file = readme.md)
   then sleep 0
   elif ! test -d $file
-  then echo "'$file' is not supposed to be there."
+  then
+    echo "'$file' is not supposed to be there."
+    all_well=false
   else
     (
       cd $file
       ls | grep '^index\.' > /dev/null
     ) ||
     echo "'$file' should contain an index file."
+    all_well=false
   fi
 done
+
+if $all_well; then
+  echo "All is well."
+fi
